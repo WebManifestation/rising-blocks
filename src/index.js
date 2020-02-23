71b88fa69746744dd45ac87b7157e58c33597eb4
Raphael Kottakal <raphael@webmanifestation.com>
2020-02-24 01:34:56 +0530

    add mic input and make visulizer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import Stats from 'stats.js';
 let container, controls;
 let camera, scene, renderer;
 let raycaster, mouse;
+let listener, analyser, audio;
+
+const cubes = [];
+const startBtn = document.getElementById('start-btn');
+const debug = document.getElementById('debug');
 
 const stats = new Stats();
 stats.domElement.style.right = 0;
@@ -14,26 +19,34 @@ document.body.appendChild(stats.dom);
 
 init();
 
+const fftSize = 512;
+
 function addItems() {
 
-  const n = 8;
+  const n = 16;
 
   for (let x = - n / 2; x < n / 2; x++) {
     for (let z = - n / 2; z < n / 2; z++) {
       // const color = new THREE.Color(`hsla(${(x + n / 2) / n * 360}, ${Math.round((x + n / 2) / n * 100)}%, ${Math.round((x + n / 2) / n * 100)}%, 1)`);
       // const colorZ = new THREE.Color(`hsla(${(z + n / 2) / n * 360}, ${Math.round((z + n / 2) / n * 100)}%, ${Math.round((z + n / 2) / n * 100)}%, 1)`);
-      const color = new THREE.Color(`hsla(${(x + n / 2) / n * 360}, 100%, 55%, 1)`);
-      const colorZ = new THREE.Color(`hsla(${(z + n / 2) / n * 360}, 100%, 55%, 1)`);
-      color.multiply(colorZ);
+      const hue = (x + n / 2) / n * 360;
+      let satu = 100;
+      let lumin = (z + n / 2) / n * 100;
+      lumin = parseInt(THREE.MathUtils.mapLinear(lumin, 0, 100, 50, 10));
+      const color = new THREE.Color(`hsla(${hue}, ${satu}%, ${lumin}%, 1)`);
+      // const colorZ = new THREE.Color(`hsla(0, ${parseInt((z + n / 2) / n * 100)}%, 100%, 1)`);
+      // color.multiply(colorZ);
       const size = 1;
       const geometry = new THREE.BoxBufferGeometry(size, size, size);
       const material = new THREE.MeshLambertMaterial({ color: color });
       const cube = new THREE.Mesh(geometry, material);
+      geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(0, size / 2, 0));
       cube.position.set(x + size / 2, 0, z + size / 2);
+      cube.scale.y = 0.1;
       cube.castShadow = true;
       cube.receiveShadow = true;
 
-      const animDuration = (Math.random() * 2000) + 1000;
+      const animDuration = (Math.random() * 100) + 200;
 
       const cubeTweenOut = new TWEEN.Tween(cube.position)
         .to({ y: 0 }, animDuration)
@@ -49,7 +62,7 @@ function addItems() {
 
       cube.tweenIn = cubeTweenIn;
       cube.tweenOut = cubeTweenOut;
-
+      cubes.push(cube);
       scene.add(cube);
     }
   }
@@ -134,9 +147,9 @@ function init() {
 
   controls = new OrbitControls(camera, renderer.domElement);
   // controls.addEventListener('change', render);
-  camera.position.set(0, 8, 16);
-  controls.enabled = false;
-  controls.autoRotate = true;
+  camera.position.set(10, 8, -16);
+  // controls.enabled = false;
+  // controls.autoRotate = true;
   // controls.minDistance = 2;
   // controls.maxDistance = 10
   // controls.target.set(0, 0, 4);
@@ -144,11 +157,34 @@ function init() {
 
   addLights();
   addItems();
+  // addAudioListener();
 
   window.addEventListener('resize', onWindowResize, false);
-  window.addEventListener('mousemove', onMouseMove, false);
-  window.addEventListener('touchmove', onMouseMove, false);
+  // window.addEventListener('mousemove', onMouseMove, false);
+  // window.addEventListener('touchmove', onMouseMove, false);
+  startBtn.addEventListener('click', addAudioListener);
   animate();
+}
+
+function addAudioListener() {
+  listener = new THREE.AudioListener();
+  listener.gain.disconnect();
+  camera.add(listener);
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
+
+}
+
+function handleSuccess(stream) {
+
+  audio = new THREE.Audio(listener);
+
+  var context = listener.context;
+  var source = context.createMediaStreamSource(stream);
+  audio.setNodeSource(source);
+
+  // audio.setVolume(0);
+
+  analyser = new THREE.AudioAnalyser(audio, fftSize);
 }
 
 function onWindowResize() {
@@ -160,6 +196,14 @@ function onWindowResize() {
 function animate() {
   stats.begin();
 
+  if (analyser) {
+    const fqData = analyser.getFrequencyData();
+    // console.log(fqData.length, cubes.length);
+    debug.innerHTML = fqData.length + ', ' + cubes.length;
+    for (let i = 0; i < fqData.length; i++) {
+      cubes[i].scale.y = (fqData[i] / 255 * 4) + 0.1;
+    }
+  }
   TWEEN.update();
   controls.update();
   renderer.render(scene, camera);
